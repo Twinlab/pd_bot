98d735611c76a3be3bea008428d51287b0f181da
Nazar Yatsyk <nazarii.yatsyk@outlook.com>
2023-04-08 17:02:23 +0300

    Minor storage.py

--- storage.py
+++ storage.py
@@ -18,26 +18,32 @@ class Users:
     def __init__(self, config) -> None:
         """Initializes storage."""
         self.config = config
-        self.users = read_json(self.config["users_file"])
 
     def add_or_update(self, new_user: dict) -> None:
         """Adds or updates user."""
         index = self._find(new_user["id"])
+        users = self._get_users()
         if index is not None:
-            self.users[index] = new_user
+            users[index] = new_user
         else:
-            self.users.append(new_user)
+            users.append(new_user)
+        write_json(self.config, users)
 
     def get_or_default(self, user_id: int):
         """Returns user by id or default user."""
         index = self._find(user_id)
         if index is not None:
-            return self.users[index]
+            users = self._get_users()
+            return users[index]
         else:
             return {"id": user_id, "links": []}
         
     def _find(self, user_id: int):
         """Returns user by id."""
-        for index, user in enumerate(self.users):
+        for index, user in enumerate(self._get_users()):
             if user["id"] == user_id:
                 return index
+
+    def _get_users(self):
+        """Returns all users."""
+        return read_json(self.config)
